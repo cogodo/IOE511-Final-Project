c6bf0cec855d8e85555d6675eaaf31ecd96882a8
Ringtail <ringtail@local>
2026-04-02 14:08:18 -0400

    Optimize 8 functions (1.4x faster)

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -8,49 +8,80 @@ from objectives.base import SolverObjective
 from options.base import SolverOptions
 
 def gradient_descent(x: Array, f: float, g: Array, objective: SolverObjective, options: SolverOptions):
-    
+
     # search direction is -g
     d = -g
 
+    # precompute g^T @ d (= -||g||^2), constant throughout this call
+    gtd = g.transpose() @ d
+
     # determine the step size
     alpha = 0
+    f_new = None
+    g_new = None
+
     match options.line_search.method:
         case 'Constant':
             alpha = options.line_search.const_alpha
+
         case 'Backtracking':
             alpha = options.line_search.alpha0
+            c1 = options.line_search.c1
+            tau = options.line_search.tau
 
             # perform backtracking line search
-            while objective.value(x + alpha*d) > f + options.line_search.c1*alpha*g.transpose() @ d:
-                alpha = alpha*options.line_search.tau
+            x_trial = x + alpha * d
+            f_trial = objective.value(x_trial)
+            while f_trial > f + c1 * alpha * gtd:
+                alpha = alpha * tau
+                x_trial = x + alpha * d
+                f_trial = objective.value(x_trial)
+
+            # We know f_new from the last evaluation
+            f_new = f_trial
 
         case 'Wolfe':
             alpha = options.line_search.alpha0
             alpha_low = options.line_search.alpha_low0
             alpha_high = options.line_search.alpha_high0
+            c1 = options.line_search.c1
+            c2 = options.line_search.c2
+            c_blend = options.line_search.c
+            c2_gtd = c2 * gtd
 
             # perform weak Wolfe line search
             while True:
-                if (objective.value(x + alpha*d) <= f + options.line_search.c1*alpha*g.transpose() @ d):
-                    if (objective.grad(x + alpha*d).transpose() @ d >= options.line_search.c2*g.transpose() @ d):
+                x_trial = x + alpha * d
+                f_trial = objective.value(x_trial)
+                if f_trial <= f + c1 * alpha * gtd:
+                    g_trial = objective.grad(x_trial)
+                    if g_trial.transpose() @ d >= c2_gtd:
+                        # Both conditions satisfied; cache results
+                        f_new = f_trial
+                        g_new = g_trial
                         break
                     alpha_low = alpha
                 else:
                     alpha_high = alpha
-                
-                alpha = options.line_search.c*alpha_low + (1 - options.line_search.c)*alpha_high
+
+                alpha = c_blend * alpha_low + (1 - c_blend) * alpha_high
 
         case _:
             raise ValueError("Line search method does not exist!")
 
-    x_new = x + alpha*d
+    x_new = x + alpha * d
+
+    if f_new is None:
+        f_new = objective.value(x_new)
+    if g_new is None:
+        g_new = objective.grad(x_new)
 
     results = StepResults(x_new=x_new,
-                          f_new=objective.value(x_new),
-                          g_new=objective.grad(x_new),
+                          f_new=f_new,
+                          g_new=g_new,
                           d=d,
                           alpha=alpha)
-    
+
     return results
 
 
--- objectives/functions.py
+++ objectives/functions.py
@@ -38,10 +38,9 @@ def rosen_Hess(x: Array):
 
 
 def quadratic_func(A: Array, b: Array, c: Array, x: Array):
-
     """ Compute function value for quadratic problems"""
-
-    return 0.5*(x.transpose() @ A @ x) + b.transpose() @ x + c
+    Ax = A @ x
+    return 0.5 * (x.transpose() @ Ax) + b.transpose() @ x + c
 
 def quadratic_grad(A: Array, b: Array, x: Array):
 
@@ -55,4 +54,3 @@ def quadratic_Hess(A: Array):
 
     return A
     
-
